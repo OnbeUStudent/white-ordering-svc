1bb8b335af7f47c61d3be6a836e4b97cc5c88614
J.R. Jenks <jrjenks@swiftprepaid.com>
2021-09-20 04:01:47 -0500

    Revert "Changed movieId from long to Guid"
    This reverts commit fe88af11c686a6c8275e19395ea5c275f00c5159.

--- test/Dii_OrderingSvc.Acceptance.Tests/Steps/CalculatorStepDefinitions.cs
+++ test/Dii_OrderingSvc.Acceptance.Tests/Steps/CalculatorStepDefinitions.cs
@@ -24,35 +24,35 @@ namespace Dii_OrderingSvc.Acceptance.Tests.Steps
            // additional string/Table parameters can be defined on the step definition
            // method. 
 
-           // _scenarioContext.Pending();
+           _scenarioContext.Pending();
        }
 
        [Given("the second number is (.*)")]
        public void GivenTheSecondNumberIs(int number)
        {
-            //TODO: implement arrange (precondition) logic
-            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
+           //TODO: implement arrange (precondition) logic
+           // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+           // To use the multiline text or the table argument of the scenario,
+           // additional string/Table parameters can be defined on the step definition
+           // method. 
 
-            // _scenarioContext.Pending();
+           _scenarioContext.Pending();
         }
-
-        [When("the two numbers are added")]
+        
+       [When("the two numbers are added")]
        public void WhenTheTwoNumbersAreAdded()
        {
-            //TODO: implement act (action) logic
+           //TODO: implement act (action) logic
 
-            // _scenarioContext.Pending();
-        }
+           _scenarioContext.Pending();
+       }
 
-        [Then("the result should be (.*)")]
+       [Then("the result should be (.*)")]
        public void ThenTheResultShouldBe(int result)
        {
-            //TODO: implement assert (verification) logic
+           //TODO: implement assert (verification) logic
 
-            // _scenarioContext.Pending();
-        }
+           _scenarioContext.Pending();
+       }
     }
 }
